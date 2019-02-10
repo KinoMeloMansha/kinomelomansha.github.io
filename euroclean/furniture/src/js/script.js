d9bf4ff2c5570b420f215abaec6d0dbe1cb7d745
kinomelomansha <kinomelomansha@gmail.com>
2019-02-11 00:39:53 +0200

    add-new

--- euroclean/furniture/src/js/script.js
+++ euroclean/furniture/src/js/script.js
@@ -1,8 +1,67 @@
 $(document).ready(function(){
   $('.video-carousel').slick({
     infinite: true,
-  slidesToShow: 2,
-   slidesToScroll: 1
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  $('.photo-carousel').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    rows: 2,
+    autoplay: true,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 
   var nav = $('.menu-header');
