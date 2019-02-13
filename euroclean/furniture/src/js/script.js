f55b4bfa89aa4c360e90788cf5cb94bc11e15b8a
kinomelomansha <kinomelomansha@gmail.com>
2019-02-14 01:03:18 +0200

    add-new

--- euroclean/furniture/src/js/script.js
+++ euroclean/furniture/src/js/script.js
@@ -3,11 +3,13 @@ $(document).ready(function(){
     infinite: true,
     slidesToShow: 2,
     slidesToScroll: 1,
+    autoplay: true,
+    pauseOnHover: true,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
           dots: true
@@ -16,7 +18,7 @@ $(document).ready(function(){
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 1
         }
       },
@@ -74,25 +76,18 @@ $(document).ready(function(){
 		}
 	});
 
-  // Add smooth scrolling on all links inside the navbar
   $("#navbarNavAltMarkup a").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
-      // Prevent default anchor click behavior
       event.preventDefault();
 
-      // Store hash
       var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
+
         window.location.hash = hash;
       });
-    }  // End if
+    }
   });
 });
